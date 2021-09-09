61a59ab71cba655b33c3756fb34c7217766c25a6
Mackensie Alvarez <mackensiealvarez@gmail.com>
2021-09-09 19:51:14 -0400

    azure-pubsub-connector:

--- src/azure-pubsub-connector.ts
+++ src/azure-pubsub-connector.ts
@@ -22,8 +22,7 @@ export class AzureConnector extends Connector {
 
     console.log('testing');
     let data = this.fetchToken();
-    console.log(data);
-    this.socket = new WebSocket('wss://asd');
+    this.socket = new WebSocket(data['url']);
     this.extendSocket();
     return this.socket;
   }
