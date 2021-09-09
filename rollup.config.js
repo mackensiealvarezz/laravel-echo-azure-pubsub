d225e8f4a31ff590f6e6a7bf126d5414be142bdc
Mackensie Alvarez <mackensiealvarez@gmail.com>
2021-09-09 18:44:12 -0400

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
     input: './src/azure-pubsub-connector.ts',
     output: [
         { file: './dist/azure-pubsub-connector.js', format: 'esm' },
         { file: './dist/azure-pubsub-connector.common.js', format: 'cjs' },
     ],
-    external: ['laravel-echo'],
     plugins: [
         typescript(),
-        resolve({
-            preferBuiltins: false
-        }),
-        commonjs(),
+        nodeResolve(),
     ],
 };
